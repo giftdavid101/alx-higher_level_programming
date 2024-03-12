2184e97b25d0fa366203ae05a0e3aaf6b8260277
giftdavid101 <giftdavid101@gmail.com>
2024-03-12 03:08:00 +0000

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+word = "Holberton"
+first_three_words = word[:3]
+last_two_words = word[-2]
+middle_word = [word[1: -1]
+print("First 3 letters:{}".formatfirst_three_words))
+print("Last 2 letters: {}".format(last_two_words))
+print("Middle word: {}".format(middle_word)
