d9464d6c68f424009dadbc8290f5dfef8ab53d0f
giftdavid101 <giftdavid101@gmail.com>
2024-03-14 21:38:41 +0000

    100-my_calculator.py

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,7 +1,41 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
 def add(a, b):
-    a = 10
-    b = 5
+    """My addition function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a + b
+    """
+    return (a + b)
 
-return (a + b)
+def sub(a, b):
+    """My subtraction function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a / b
+    """
+    return int(a / b)
