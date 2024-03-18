d626777209d7b112713dd3a50ca3423fad94e92d
giftdavid101 <giftdavid101@gmail.com>
2024-03-18 21:17:44 +0000

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,5 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
+    elif !my_list:
+        return my_list
     else:
         return print(my_list[:idx] + my_list[idx + 1:])
