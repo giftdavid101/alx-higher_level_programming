9e0f6f8d5661013fa10447d7c66dd71acb54feb1
giftdavid101 <giftdavid101@gmail.com>
2024-04-08 12:30:34 +0000

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-"""Create an empty class called Rectangle"""
+"""Class Rectangle"""
 
 
 class Rectangle:
-    """Empty rectangle class"""
+    """rectangle class"""
 
     pass
