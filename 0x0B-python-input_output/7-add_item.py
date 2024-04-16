b8fdd4574aa4802ed5286350de539dd71a5418a6
giftdavid101 <giftdavid101@gmail.com>
2024-04-16 13:14:50 +0000

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""fills list and saves to json file"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    allargs = load_from_json_file("add_item.json")
+except Exception:
+    allargs = []
+
+allargs.extend(sys.argv[1:])
+save_to_json_file(allargs, "add_item.json")
