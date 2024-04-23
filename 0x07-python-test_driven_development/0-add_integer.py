8541f95fc976f38a82aa63577098f50a3773bd91
giftdavid101 <giftdavid101@gmail.com>
2024-04-23 11:14:14 +0000

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+"""
+A function that adds two numbers.
+
+Parameters:
+a (int): first number.
+b (int): second number.
+
+Returns:
+int: The sum of a and b.
+"""
+    return a+b;
