5da995981b772802d3bdb6a64b4b37986279aaa9
giftdavid101 <giftdavid101@gmail.com>
2024-04-23 11:38:08 +0000

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,10 +11,15 @@ b (int): second number.
 Returns:
 int: The sum of a and b.
 """
-    if type(a) && type(b) is not int and type(a) && type(b) is not float:
-        raise TypeError("a and b must be an integer")
-    a = int(a)
-    b = int(b)
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
 
 
     return a+b;
