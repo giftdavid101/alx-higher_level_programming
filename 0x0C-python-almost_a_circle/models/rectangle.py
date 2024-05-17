4bae99b47baa52c0b585ecf8910e02a6a9792abc
giftdavid101 <giftdavid101@gmail.com>
2024-05-17 19:29:58 +0000

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -43,14 +43,3 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         self.__x = value
-
-if __name__ == "__main__":
-
-    r1 = Rectangle(10, 2)
-    print(r1.id)
-
-    r2 = Rectangle(2, 10)
-    print(r2.id)
-
-    r3 = Rectangle(10, 2, 0, 0, 12)
-    print(r3.id)
