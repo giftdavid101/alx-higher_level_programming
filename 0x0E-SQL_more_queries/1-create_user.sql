60aa9832120a9138db7fafc5cf4f65b3402bbc88
giftdavid101 <giftdavid101@gmail.com>
2024-05-23 14:59:37 +0100

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Script creates the database and the user
+-- Script creates the mysql server user
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
