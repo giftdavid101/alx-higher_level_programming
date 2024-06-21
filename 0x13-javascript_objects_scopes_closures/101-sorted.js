1280dd99c712383087201e3f80a4c70ae68da7ee
giftdavid101 <giftdavid101@gmail.com>
2024-06-21 20:27:10 +0100

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,9 +3,9 @@
 const { dict } = require('./101-data.js');
 const hashT = {};
 for (const i in dict) {
-    if (hashT[dict[i]] === undefined) {
-	hashT[dict[i]] = [];
-    }
-    hashT[dict[i]].push(i);
+  if (hashT[dict[i]] === undefined) {
+    hashT[dict[i]] = [];
+  }
+  hashT[dict[i]].push(i);
 }
 console.log(hashT);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const src1 = fs.readFileSync(process.argv[2], 'utf8');
-const src2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], src1 + src2);
+
+if (
+  fs.existsSync(process.argv[2]) &&
+fs.statSync(process.argv[2]).isFile &&
+fs.existsSync(process.argv[3]) &&
+fs.statSync(process.argv[3]).isFile &&
+fileC !== undefined
+) {
+  const fileOne = fs.readFileSync(process.argv[2]);
+  const fileTwo = fs.readFileSync(process.argv[3]);
+  const stream = fs.createWriteStream(process.argv[4]);
+
+  stream.write(fileOne);
+  stream.write(fileTwo);
+  stream.end();
+}
