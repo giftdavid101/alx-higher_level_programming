366f5cede807ced55011f29adab893056a223636
giftdavid101 <giftdavid101@gmail.com>
2024-06-21 20:34:31 +0100

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 const fs = require('fs');
 
+
+const A = process.argv[2];
+const B = process.argv[3];
+const C = process.argv[4];
+
 if (
-  fs.existsSync(process.argv[2]) &&
-fs.statSync(process.argv[2]).isFile &&
-fs.existsSync(process.argv[3]) &&
-fs.statSync(process.argv[3]).isFile &&
+  fs.existsSync(A) &&
+fs.statSync(A).isFile &&
+fs.existsSync(B) &&
+fs.statSync(B).isFile &&
 fileC !== undefined
 ) {
-  const fileOne = fs.readFileSync(process.argv[2]);
-  const fileTwo = fs.readFileSync(process.argv[3]);
-  const stream = fs.createWriteStream(process.argv[4]);
+  const fileOne = fs.readFileSync(A);
+  const fileTwo = fs.readFileSync(B);
+  const stream = fs.createWriteStream(C);
   stream.write(fileOne);
   stream.write(fileTwo);
   stream.end();
