691fbaee600b37a36a3afd37d6f475cf977749fd
giftdavid101 <giftdavid101@gmail.com>
2024-07-05 03:42:03 +0100

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script sends a JSON POST request to a given URL with a given JSON file.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"i
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
